87b154dede92f03f8e0887d241762495c41e1591
Rémi B <remibouguermouh@gmail.com>
2017-04-28 08:41:14 +0200

    Add update service storage
    Ajout de la fonction update

--- src/providers/storage.ts
+++ src/providers/storage.ts
@@ -1,9 +1,9 @@
-import {Injectable} from '@angular/core';
-import {Http} from '@angular/http';
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
 import 'rxjs/add/operator/map';
-import {Storage} from "@ionic/storage";
-import {BehaviorSubject} from "rxjs";
-import {Entry} from "../model/entry.class"
+import { Storage } from "@ionic/storage";
+import { BehaviorSubject } from "rxjs";
+import { Entry } from "../model/entry.class"
 
 /*
  Generated class for the Store provider.
@@ -69,10 +69,26 @@ export class StoreService {
 
 
   saveEntry(e: Entry) {
-    console.log('saving entry ...');
     let cacheArr = this.entries.getValue();
     cacheArr.push(e);
     this.entries.next(cacheArr);
+  }
+
+  /**
+   * Update un index
+   * retourne true si reussit, faux si echec
+   */
+  update(index: number, e: Entry) :boolean {
+    // On vérifie si l'entrée que l'ont veut modifier existe dans la liste
+    if (this.getEntry(index)) {
+      console.log('in')
+      const list = this.entries.getValue();
+      list[index] = e;
+      this.entries.next(list);
+      return true;
+    } else {
+      return false;
+    }
   }
 
   clear() {
